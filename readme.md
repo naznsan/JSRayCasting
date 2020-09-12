97c82667c1db9755fcb06b831ce1b73b8e8b0b1c
Justin Kim <naznsan@gmail.com>
2020-09-12 16:05:05 +1200

    Initialize files and create canvas

---
+++ readme.md
@@ -0,0 +1,6 @@
+# JSRayCasting
+A simple ray-casting demo made with JavaScript.
+
+## Versions
+### v 0.1
+Initialized files and created canvas
---
+++ script.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello, World!", 700, 500);
